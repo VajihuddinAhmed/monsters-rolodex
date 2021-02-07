db398ee0a6564f2481e414214d4b08f404291c2d
VajihuddinAhmed <vajihahmed@gmail.com>
2021-02-07 04:47:52 +0100

    completed small project by using react & adding files to github pages

---
+++ src/components/search-box/search-box.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './search-box.css';
+
+export const SearchBox = (props) => {
+    return (
+        <input 
+            type='search' 
+            placeholder={props.placeholder} 
+            onChange={props.handleChange} 
+            className='search'
+        />
+    )
+}
